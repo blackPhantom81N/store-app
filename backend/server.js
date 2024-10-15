0f66e45bb2cd67697d881045e269dbec46925608
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-10-15 23:12:06 +0530

    Added auth controllers

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectToDatabase from "./config/db.js";
 import Product from "./models/product.model.js";
+import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.routes.js";
 import productRoute from "./routes/product.routes.js";
+import connectToDatabase from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //allows JSON data in the req.body
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoute);
 
 app.listen(5000, function () {
