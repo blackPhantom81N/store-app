baafb1235724ea8af51da176b76da4a594368c33
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-11-07 09:47:17 +0530

    Added changes to Login

---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { useToast } from "@chakra-ui/react";
+
+interface LoginTypes {
+  username: string;
+  password: string;
+}
+
+const useLogin = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { setAuthUser } = useAuthContext();
+  const toast = useToast();
+  const login = async ({ username, password }: LoginTypes) => {
+    const success = handleInputErrors(username, password, toast);
+    if (!success) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/JSON" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+      if (!data) {
+        throw new Error(data?.error);
+      }
+
+      localStorage.setItem("authUser", JSON.stringify(data));
+      setAuthUser(data);
+    } catch (error) {
+      console.error("Error occured in login-catch ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, login };
+};
+
+export default useLogin;
+
+const handleInputErrors = (
+  username: string,
+  password: string,
+  toast: ReturnType<typeof useToast>
+) => {
+  if (!username || !password) {
+    toast({
+      title: "Error",
+      description: "Please fill all the fields",
+      status: "error",
+      duration: 9000,
+      isClosable: true,
+    });
+    return false;
+  }
+  return true;
+};
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,33 @@
 //username, password
 
+import React, { FormEvent, useState } from "react";
+import useLogin from "../hooks/useLogin";
+
+interface LoginDetails {
+  username: string;
+  password: string;
+}
+
 const Login = () => {
-  return <div>Login</div>;
+  const [loginDetails, setLoginDetails] = useState<LoginDetails>({
+    username: "",
+    password: "",
+  });
+
+  const { login, loading } = useLogin();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await login(loginDetails);
+  };
 };
 
 export default Login;
